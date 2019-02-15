e3a50a4f809f215e799ef0a4c9735ab020090c8c
CatB1794 <46718246+CatB1794@users.noreply.github.com>
2019-02-15 13:06:07 +0000

    Complete SQL code
    I think it's complete - still need to test

--- SQL code.sql
+++ SQL code.sql
@@ -1,79 +1,95 @@
 --All the countries in the world organised by largest population to smallest:
-SELECT name FROM country
+SELECT code, name, continent, region, population, capital FROM country
 ORDER BY population DESC
 
 --All the countries in a continent organised by largest population to smallest:
-SELECT name FROM country
+SELECT code, name, continent, region, population, capital FROM country
 ORDER BY continent, population DESC
 
 --All the countries in a region organised by largest population to smallest:
-SELECT name FROM country
+SELECT code, name, continent, region, population, capital FROM country
 ORDER BY region, population DESC
 
 --The top N populated countries in the world where N is provided by the user:
-
+SELECT code, name, continent, region, population, capital FROM country LIMIT 
+ORDER BY population DESC
 
 --The top N populated countries in a region where N is provided by the user:
-
+SELECT code, name, continent, region, population, capital FROM country LIMIT 
+ORDER BY region, population DESC
 
 --All the cities in the world organised by largest population to smallest:
-SELECT name FROM city
+SELECT name, country.name, district, population FROM city
+JOIN country ON CountryCode = country.code 
 ORDER BY population DESC
 
 --All the cities in a continent organised by largest population to smallest:
-SELECT name FROM city
+SELECT name, country.name, district, population FROM city
 JOIN country ON CountryCode = country.code
 ORDER BY continent, population DESC
 
 --All the cities in a region organised by largest population to smallest:
-SELECT name FROM city
+SELECT name, country.name, district, population FROM city
 JOIN country ON CountryCode = country.code
 ORDER BY region, population DESC
 
 --All the cities in a country organised by largest population to smallest:
-SELECT name FROM city
-JOIN country ON CountryCode = country.code
-ORDER BY country.name, population DESC
+SELECT name, country.name, district, population FROM city
+JOIN country ON CountryCode = country.code 
+ORDER BY country, population DESC
 
 --All the cities in a district organised by largest population to smallest:
-SELECT name FROM city
+SELECT name, country.name, district, population FROM city
+JOIN country ON CountryCode = country.code 
 ORDER BY district, population DESC
 
 --The top N populated cities in the world where N is provided by the user:
-
+SELECT name, country.name, district, population FROM city LIMIT 
+JOIN country ON CountryCode = country.code 
+ORDER BY population DESC
 
 --The top N populated cities in a continent where N is provided by the user:
-
+SELECT name, country.name, district, population FROM city LIMIT
+JOIN country ON CountryCode = country.code 
+ORDER BY continent, population DESC
 
 --The top N populated cities in a region where N is provided by the user:
-
+SELECT name, country.name, district, population FROM city LIMIT
+JOIN country ON CountryCode = country.code 
+ORDER BY region, population DESC
 
 --The top N populated cities in a country where N is provided by the user:
-
+SELECT name, country.name, district, population FROM city LIMIT
+JOIN country ON CountryCode = country.code 
+ORDER BY country, population DESC
 
 --The top N populated cities in a district where N is provided by the user:
-
+SELECT name, country.name, district, population FROM city LIMIT 
+ORDER BY district, population DESC
 
 --All the capital cities in the world organised by largest population to smallest:
-SELECT capital FROM country
+SELECT capital, name, population FROM country
 ORDER BY population DESC
 
 --All the capital cities in a continent organised by largest population to smallest:
-SELECT capital FROM country
+SELECT capital, name, population FROM country
 ORDER BY continent, population DESC
 
 --All the capital cities in a region organised by largest to smallest:
-SELECT capital FROM country
+SELECT capital, name, population FROM country
 ORDER BY region, population DESC
 
 --The top N populated capital cities in the world where N is provided by the user:
-
+SELECT capital, name, population FROM country LIMIT 
+ORDER BY population DESC
 
 --The top N populated capital cities in a continent where N is provided by the user:
-
+SELECT capital, name, population FROM country LIMIT 
+ORDER BY continent, population DESC
 
 --The top N populated capital cities in a region where N is provided by the user:
-
+SELECT capital, name, population FROM country LIMIT 
+ORDER BY region, population DESC
 
 --The population of people, people living in cities, and people not living in cities in each continent:
 SELECT population, city.population, SUM(population - city.population) FROM country
