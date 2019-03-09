cfa333818d3f031d072144eb37114b989d58945c
Ross Kelly <rsjkd12@gmail.com>
2019-03-09 21:49:11 +0000

    Adding an initial version of the country object

--- src/main/java/com/napier/sem/DatabaseObjects/Country.java
+++ src/main/java/com/napier/sem/DatabaseObjects/Country.java
@@ -1,4 +1,87 @@
 package com.napier.sem.DatabaseObjects;
 
+/**
+ * Country class, same as in the database.
+ */
 public class Country {
+
+    public Country(){
+    }
+
+    /**
+     * The country code
+     */
+    public String Code;
+
+    /**
+     * The country name
+     */
+    public String Name;
+
+
+    ///** This will probably get changed... .. Not the best way to do it*/
+    //public enum Continents { ASIA, EUROPE;}
+    /** Country continent -> Could possibly be an enum as above */
+    public String Continent;
+
+    /**
+     * Country Region
+     */
+    public String Region;
+
+    /**
+     * Country SurfaceArea
+     */
+    public Float SurfaceArea;
+
+    /**
+     * Country IndepYear
+     */
+    public int IndepYear;
+
+    /**
+     * Country Population
+     */
+    public int Population;
+
+    /**
+     * Country LifeExpectancy
+     */
+    public Float LifeExpectancy;
+
+    /**
+     * Country GNP
+     */
+    public Float GNP;
+
+    /**
+     * Country GNPOld
+     */
+    public Float GNPOld;
+
+    /**
+     * Country LocalName
+     */
+    public String LocalName;
+
+    /**
+     * Country GovernmentForm
+     */
+    public String GovernemntForm;
+
+    /**
+     * Country HeadOfState
+     */
+    public String HeadOfState;
+
+    /**
+     * Country Capital
+     * In form of ID number
+     */
+    public int Capital;
+
+    /**
+     * Country 2 letter code
+     */
+    public String Code2;
 }
