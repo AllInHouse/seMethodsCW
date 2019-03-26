cd1850e7ed196edd999971ee389bc3324c291da8
Ross Kelly <rsjkd12@gmail.com>
2019-03-26 13:12:05 +0000

    Example spring application -> Hopefully doesn't build. Might change our code coverage percentage

---
+++ src/main/java/springExample/SpringExampleApp.java
@@ -0,0 +1,12 @@
+package springExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringExampleApp.class, args);
+    }
+}
